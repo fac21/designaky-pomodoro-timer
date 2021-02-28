8e6c0da6bf0a5a8517e7a6cdf285475ad2c72ecb
designaky <designaky@gmail.com>
2021-02-28 23:34:17 +0000

    Adding set interval and some CSS

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const currentDate = document.getElementById("date");
 const currentTime = document.getElementById("time");
 
+const startPausebtn = document.getElementById("pause--btn");
+
 // Date section and time
 
 const getTime = () => {
@@ -20,10 +22,20 @@ const getTime = () => {
     "November",
     "December",
   ];
-  currentDate.innerText = `${cDate.getDate()} ${
-    monthsLong[cDate.getMonth()]
-  } ${cDate.getFullYear()}`;
-  currentTime.innerText = `${cDate.getHours()}:${cDate.getMinutes()}:${cDate.getSeconds()}`;
+
+  if (currentDate.innerText === "") {
+    currentDate.innerText = `${cDate.getDate()} ${
+      monthsLong[cDate.getMonth()]
+    } ${cDate.getFullYear()}`;
+  }
+
+  currentTime.innerText = `${formatTime(cDate.getHours())}:${formatTime(
+    cDate.getMinutes()
+  )}:${formatTime(cDate.getSeconds())}`;
+};
+
+const formatTime = (time) => {
+  return time < 10 ? `0${time}` : time;
 };
 
 getTime();
@@ -39,3 +51,19 @@ const playSound = () => {
   }
 };
 */
+
+// Countdown
+
+const countdown = () => console.log("Hello");
+
+const startPauseFunction = () => {
+  if (startPausebtn.innerHTML === "Start") {
+    let intervalStart = setInterval(countdown, 1000);
+    startPausebtn.innerHTML = "Pause";
+  } else {
+    clearInterval(intervalStart);
+    startPausebtn.innerHTML = "Start";
+  }
+};
+
+startPausebtn.addEventListener("click", startPauseFunction);
